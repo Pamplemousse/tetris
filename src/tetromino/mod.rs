61fd374ae5ff8743e1872045fb41536e63d20ce2
Pamplemousse <xav.maso@gmail.com>
2020-01-16 15:34:39 -0700

    Don't let the tetrimino move past the side borders

--- src/tetromino/mod.rs
+++ src/tetromino/mod.rs
@@ -25,15 +25,33 @@ impl Tetromino {
     }
 
     pub fn move_left(&mut self) {
-        self.position.x -= ATOM_SIZE as i32;
+        let new_x = self.position.x - ATOM_SIZE as i32;
+
+        if new_x >= 0 {
+            self.position.x = new_x;
+        }
     }
 
     pub fn move_right(&mut self) {
-        self.position.x += ATOM_SIZE as i32;
+        let new_x = self.position.x + ATOM_SIZE as i32;
+
+        if new_x as u32 + self.width() <= WINDOW_WIDTH {
+            self.position.x = new_x;
+        }
     }
 
     pub fn new(x :i32, y :i32, shape :Shape) -> Tetromino {
         let position = Position { x, y };
         Tetromino { shape, position }
+    }
+
+    fn width(&self) -> u32 {
+        let width_in_number_of_atoms = match self.shape {
+            Shape::I => 1,
+            Shape::J | Shape::L | Shape::O => 2,
+            Shape::S | Shape::T | Shape::Z => 3
+        };
+
+        width_in_number_of_atoms * ATOM_SIZE
     }
 }
